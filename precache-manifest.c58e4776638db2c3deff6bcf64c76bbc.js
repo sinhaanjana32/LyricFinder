89830628a33ebde7eec963a1e9f7a048820714c5
Anjana <sinhaanjana32@gmail.com>
2020-07-23 00:18:01 +0300

    Updates

---
+++ precache-manifest.c58e4776638db2c3deff6bcf64c76bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19bff4157107319d21bb73e06e35fda",
+    "url": "/LyricFinder/index.html"
+  },
+  {
+    "revision": "4390a9b46a3d1719040d",
+    "url": "/LyricFinder/static/css/main.705587de.chunk.css"
+  },
+  {
+    "revision": "2ff7bfd669d34a8be707",
+    "url": "/LyricFinder/static/js/2.1640ee29.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/LyricFinder/static/js/2.1640ee29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4390a9b46a3d1719040d",
+    "url": "/LyricFinder/static/js/main.8ada2bb9.chunk.js"
+  },
+  {
+    "revision": "f0b8de95d84061485406",
+    "url": "/LyricFinder/static/js/runtime-main.4b0ad60e.js"
+  }
+]);
